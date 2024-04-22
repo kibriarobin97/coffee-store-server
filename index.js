f65ca951fc51ff1ab8678ec88c31c91887246bb4
kibriarobin97 <kibriarobin97@gmail.com>
2024-04-22 17:04:01 +0600

    added update functionality

--- index.js
+++ index.js
@@ -42,8 +42,11 @@ async function run() {
     })
 
 
-    app.get('/coffee/:id', (req, res) => {
-        
+    app.get('/coffee/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId (id)}
+        const result = await coffeeCollection.findOne(query)
+        res.send(result)
     })
 
 
@@ -51,6 +54,27 @@ async function run() {
         const newCoffee = req.body
         console.log(newCoffee)
         const result = await coffeeCollection.insertOne(newCoffee)
+        res.send(result)
+    })
+
+
+    app.put('/coffee/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id : new ObjectId(id)}
+        const options = { upsert: true };
+        const updatedCoffee = req.body;
+        const coffee = {
+            $set: {
+                name: updatedCoffee.name,
+                quantity: updatedCoffee.quantity,
+                supplier: updatedCoffee.supplier,
+                taste: updatedCoffee.taste,
+                category: updatedCoffee.category,
+                details: updatedCoffee.details,
+                photo: updatedCoffee.photo
+            }
+        }
+        const result = await coffeeCollection.updateOne(filter, coffee, options)
         res.send(result)
     })
 
